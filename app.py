b252b7a1b929830741ed310727b19dfa529e1950
LadinaSch <ladina.schustereit@gmail.com>
2025-11-17 17:14:22 +0100

    kalender hinzugefügt

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
-from datetime import date
+from datetime import datetime, time
 
 st.title('Arbeitsplan ')
 st.divider()
+
+#kalender welcher angezeigt wird 
+date_selected = st.date_input("Datum auswählen:")
+time_selected = st.time_input("Zeit auswählen:", value=time(12, 0))
+
+datetime_selected = datetime.combine(date_selected, time_selected)
+
+st.write("Gewählter Zeitpunkt:", datetime_selected)
+
+
+
 
